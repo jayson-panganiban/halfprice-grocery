51a1d495a4517eac1fd7d16a4b419853a8f4fb1f
Jayson <jsoncp@proton.me>
2024-10-22 15:50:17 +1100

    feat: loading spinner

---
+++ frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/LoadingSpinner.css';
+
+const LoadingSpinner = React.memo(() => (
+  <div className="loading-spinner-container">
+    <div className="loading-spinner" aria-label="Loading content"></div>
+    <p className="loading-text">Loading...</p>
+  </div>
+));
+
+LoadingSpinner.displayName = 'LoadingSpinner';
+
+export default LoadingSpinner;
